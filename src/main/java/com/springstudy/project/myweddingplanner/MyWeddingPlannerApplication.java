e364dc2f373749060dc34b0771e3d69e2812b128
Jaeyoon <wodbs4338@naver.com>
2021-05-03 01:17:07 +0900

    Spring Boot, JPA, Thymeleaf, MySQL 환경 구성

---
+++ src/main/java/com/springstudy/project/myweddingplanner/MyWeddingPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.project.myweddingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWeddingPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWeddingPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springstudy/project/myweddingplanner/MyWeddingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.project.myweddingplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWeddingPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
