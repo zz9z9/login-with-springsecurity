6328443074c684deba9cb1fec436476b7beb95aa
Jaeyoon <wodbs4338@naver.com>
2021-05-04 18:39:35 +0900

    open id 적용

--- src/main/java/com/springstudy/project/myweddingplanner/config/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/springstudy/project/myweddingplanner/config/OAuth2LoginSecurityConfig.java
@@ -2,32 +2,29 @@ package com.springstudy.project.myweddingplanner.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@EnableWebSecurity
 @Configuration
 public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         Set<String> googleScopes = new HashSet<>();
-        googleScopes.add(
-                "https://www.googleapis.com/auth/userinfo.email");
-        googleScopes.add(
-                "https://www.googleapis.com/auth/userinfo.profile");
+        googleScopes.add("https://www.googleapis.com/auth/userinfo.email");
+        googleScopes.add("https://www.googleapis.com/auth/userinfo.profile");
 
         OidcUserService googleUserService = new OidcUserService();
         googleUserService.setAccessibleScopes(googleScopes);
 
         http
                 .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .mvcMatchers("/home").permitAll()
                         .anyRequest().authenticated())
-                .oauth2Login(oauthLogin -> oauthLogin
+                    .oauth2Login(oauthLogin -> oauthLogin
                         .userInfoEndpoint()
                         .oidcUserService(googleUserService));
     }
--- src/main/java/com/springstudy/project/myweddingplanner/controller/LoginController.java
+++ src/main/java/com/springstudy/project/myweddingplanner/controller/LoginController.java
@@ -3,13 +3,23 @@ package com.springstudy.project.myweddingplanner.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    @GetMapping("/home")
+    public String index(){
+        return "index";
+    }
 
-    @GetMapping("/oidc-principal")
-    public OidcUser getOidcUserPrincipal(@AuthenticationPrincipal OidcUser principal) {
-        return principal;
+    @GetMapping("/user")
+    public String user(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
+        System.out.println(oidcUser.getIdToken().getTokenValue());
+        System.out.println("email : " + oidcUser.getUserInfo().getEmail());
+        model.addAttribute("userName", oidcUser.getName());
+        model.addAttribute("audience", oidcUser.getAudience());
+        return "user";
     }
 }
+  
