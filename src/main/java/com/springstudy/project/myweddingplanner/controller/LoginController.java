0ace6cc14c419fd5670b16320b8d2e245706da33
Jaeyoon <wodbs4338@naver.com>
2021-05-06 22:54:36 +0900

    LoginController, MemberController 수정

--- src/main/java/com/springstudy/project/myweddingplanner/controller/LoginController.java
+++ src/main/java/com/springstudy/project/myweddingplanner/controller/LoginController.java
@@ -1,13 +1,11 @@
 package com.springstudy.project.myweddingplanner.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
     @GetMapping("/")
     public String home(){
         return "index";
@@ -16,15 +14,6 @@ public class LoginController {
     @GetMapping("/login")
     public String login(){
         return "login";
-    }
-
-    @GetMapping("/user")
-    public String user(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
-        System.out.println(oidcUser.getIdToken().getTokenValue());
-        System.out.println("email : " + oidcUser.getUserInfo().getEmail());
-        model.addAttribute("userName", oidcUser.getName());
-        model.addAttribute("audience", oidcUser.getAudience());
-        return "user";
     }
 }
   
--- src/main/java/com/springstudy/project/myweddingplanner/controller/MemberController.java
+++ src/main/java/com/springstudy/project/myweddingplanner/controller/MemberController.java
@@ -1,15 +1,20 @@
 package com.springstudy.project.myweddingplanner.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
     @GetMapping("/main")
-    public String main() {
+    public String userMain(@AuthenticationPrincipal OAuth2User principal) {
+        System.out.println("principal = " + principal);
         return "main";
     }
 }
