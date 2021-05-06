52fe28144087e45f96dc386f3c54a771d0539ec1
Jaeyoon <wodbs4338@naver.com>
2021-05-06 23:00:09 +0900

    SecurityConfig 파일에서 login 성공시, logout시 리다이렉트 url 설정

--- src/main/java/com/springstudy/project/myweddingplanner/config/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/springstudy/project/myweddingplanner/config/OAuth2LoginSecurityConfig.java
@@ -2,42 +2,25 @@ package com.springstudy.project.myweddingplanner.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Configuration
+@EnableWebSecurity
 public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private OidcUserService getgoogleUserService() {
-        Set<String> googleScopes = new HashSet<>();
-        googleScopes.add("https://www.googleapis.com/auth/userinfo.email");
-        googleScopes.add("https://www.googleapis.com/auth/userinfo.profile");
-
-        OidcUserService googleUserService = new OidcUserService();
-        googleUserService.setAccessibleScopes(googleScopes);
-
-        return googleUserService;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/login","/").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .oauth2Login()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/member/main")
-                    .and()
-                .logout()
-                    .logoutSuccessUrl("/")
-                    .and()
-                .oauth2Login()
-                    .userInfoEndpoint()
-                    .oidcUserService(getgoogleUserService());
+                .antMatchers("/login", "/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .oauth2Login()
+                        .loginPage("/login")
+                            .defaultSuccessUrl("/member/main")
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/");
     }
 }
