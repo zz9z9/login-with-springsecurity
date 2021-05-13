65e4c8e3c7c78a6956557014dc2363f9a4298f46
Jaeyoon <wodbs4338@naver.com>
2021-05-13 23:27:25 +0900

    PasswordEncoder bean securityConfig로 이동

--- src/main/java/com/springstudy/project/myweddingplanner/config/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/springstudy/project/myweddingplanner/config/OAuth2LoginSecurityConfig.java
@@ -2,15 +2,16 @@ package com.springstudy.project.myweddingplanner.config;
 
 import com.springstudy.project.myweddingplanner.security.jwt.JwtAuthenticationFilter;
 import com.springstudy.project.myweddingplanner.security.jwt.JwtProvider;
-import com.springstudy.project.myweddingplanner.service.CustomOAuth2UserService;
+import com.springstudy.project.myweddingplanner.security.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
@@ -20,18 +21,18 @@ public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
     final private JwtProvider jwtProvider;
 
-    // authenticationManager를 Bean 등록합니다.
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic()
+                    .disable() // rest api 만을 고려하여 기본 설정은 해제하겠습니다.
+                .csrf()
+                    .disable() // csrf 보안 토큰 disable처리.
+//                .sessionManagement()
+//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지 않습니다.
+//                .and()
                 .authorizeRequests()
-                     // .antMatchers("/login", "/").permitAll()
+                    .antMatchers("/main/**").authenticated()
                     .anyRequest().permitAll()
                 .and()
                     .oauth2Login()
@@ -49,6 +50,11 @@ public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/login", "/");
+         web.ignoring().antMatchers("/login", "/member/signup","/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
