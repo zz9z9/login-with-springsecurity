8ba25e19850bd49bf01b9a9202a1070edbfde235
Jaeyoon <wodbs4338@naver.com>
2021-05-18 01:36:23 +0900

    Jwt Filter에서 accessToken 만료되었을 시, refreshToken 확인하여 유효한 경우 재발급 하도록 수정

--- src/main/java/com/springstudy/project/myweddingplanner/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/springstudy/project/myweddingplanner/security/jwt/JwtAuthenticationFilter.java
@@ -2,38 +2,57 @@ package com.springstudy.project.myweddingplanner.security.jwt;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtProvider jwtProvider;
+    private final JwtManager jwtManager;
     // private final AuthenticationManager authenticationManager = new ProviderManager();
 
-    public JwtAuthenticationFilter(JwtProvider jwtProvider) {
-        this.jwtProvider = jwtProvider;
+    public JwtAuthenticationFilter(JwtManager jwtManager) {
+        this.jwtManager = jwtManager;
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("JwtAuthenticationFilter.doFilter");
-        String token = jwtProvider.resolveToken((HttpServletRequest) request); // 헤더에서 JWT 를 받아옵니다.
+        String accessToken = jwtManager.getTokenByKey(request, JwtManager.ACCESS_TOKEN_NAME);
+        String refreshToken = jwtManager.getTokenByKey(request, JwtManager.REFRESH_TOKEN_NAME);
 
-        // 유효한 토큰인지 확인합니다.
-        if (token != null && jwtProvider.validateToken(token)) {
-            Authentication authentication = jwtProvider.getAuthentication(token); // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
-            SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContext 에 Authentication 객체를 저장합니다.
+        // accessToken 유효한지 확인
+        if(accessToken!=null) {
+            if(jwtManager.isNotExpired(accessToken)) {
+                Authentication authentication = jwtManager.getAuthentication(accessToken); // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
+                SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContext 에 Authentication 객체를 저장합니다.
+            } else { // accessToken 만료되었으면 refreshToken 확인
+                if(jwtManager.isNotExpired(refreshToken)) {
+                    String userIdentifier = jwtManager.getUserIdentifier(accessToken);
+                    String newAccessToken = jwtManager.generateAccessToken(userIdentifier, new HashMap<>());
 
-            System.out.println("authentication : "+authentication);
+                    setCookie(response, JwtManager.ACCESS_TOKEN_NAME, newAccessToken);
+
+                    Authentication authentication = jwtManager.getAuthentication(newAccessToken); // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
+                    SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContext 에 Authentication 객체를 저장합니다.
+                }
+            }
         }
 
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
+    }
+
+    private void setCookie(HttpServletResponse response, String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
     }
 }
