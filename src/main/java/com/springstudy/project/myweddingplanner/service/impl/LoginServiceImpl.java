0b51694c9b86a25d092e6bff1aafa672673f387e
Jaeyoon <wodbs4338@naver.com>
2021-05-17 23:57:54 +0900

    로그인 성공시 accessToken, refreshToken 생성

--- src/main/java/com/springstudy/project/myweddingplanner/service/impl/LoginServiceImpl.java
+++ src/main/java/com/springstudy/project/myweddingplanner/service/impl/LoginServiceImpl.java
@@ -1,10 +1,11 @@
 package com.springstudy.project.myweddingplanner.service.impl;
 
 import com.springstudy.project.myweddingplanner.dto.MemberDTO;
-import com.springstudy.project.myweddingplanner.security.jwt.JwtProvider;
+import com.springstudy.project.myweddingplanner.security.jwt.JwtManager;
 import com.springstudy.project.myweddingplanner.service.spec.LoginService;
 import com.springstudy.project.myweddingplanner.service.spec.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +19,9 @@ import java.util.Map;
 public class LoginServiceImpl implements LoginService {
 
     private final PasswordEncoder passwordEncoder;
-    private final JwtProvider jwtProvider;
+    private final JwtManager jwtManager;
     private final MemberService memberService;
-    // private final MemberRepository memberRepository;
+    private final RedisTemplate redisTemplate;
 
     @Override
     public void processOAuthLoginSuccess(MemberDTO member, HttpServletResponse response) {
@@ -28,13 +29,19 @@ public class LoginServiceImpl implements LoginService {
             // TODO : OAuth Resource Server 에서 가져오는 정보에 이메일 없는 경우 사용자가 수동으로 입력하도록 해야한다.
         } else {
             MemberDTO findMember = memberService.find(member.getEmail());
-
             if(findMember==null) {
                 memberService.register(member);
                 findMember = member;
             }
 
-            setJwtCookie(findMember, response);
+            Map<String, Object> params = new HashMap<>();
+            params.put("name", findMember.getName());
+
+            String accessToken = jwtManager.getAccessToken(findMember.getEmail(), params);
+            String refreshToken = jwtManager.getRefreshToken();
+
+            setCookie(response, "accessToken", accessToken);
+            setCookie(response, "refreshToken", refreshToken);
         }
     }
 
@@ -44,24 +51,28 @@ public class LoginServiceImpl implements LoginService {
         boolean checkPw = passwordEncoder.matches(member.getPassword(), findMember.getPassword());
 
         if(findMember!=null && checkPw) {
-            setJwtCookie(findMember, response);
+            Map<String, Object> params = new HashMap<>();
+            params.put("name", member.getName());
+
+            String accessToken = jwtManager.getAccessToken(member.getEmail(), params);
+            String refreshToken = jwtManager.getRefreshToken();
+
+            setCookie(response, "accessToken", accessToken);
+            setCookie(response, "refreshToken", refreshToken);
+
+            // redisUtil.setData(findMember.getEmail(), ); // refreshToken 유효 시간
         } else {
             // TODO : 잘못된 사용자 또는 비밀번호 입니다.
+            response.setStatus(500);
         }
     }
 
-    private void setJwtCookie(MemberDTO member, HttpServletResponse response) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("name", member.getName());
+    private void setCookie(HttpServletResponse response, String key, String value) {
+         Cookie cookie = new Cookie(key, value);
 
-        String jwt = jwtProvider.createToken(member.getEmail(), params);
-
-        if(jwt!=null) {
-            Cookie cookie = new Cookie("jwt", jwt);
-            cookie.setPath("/");
-            cookie.setHttpOnly(true);
-
-            response.addCookie(cookie);
-        }
+         cookie.setPath("/");
+         cookie.setHttpOnly(true);
+         response.addCookie(cookie);
     }
+
 }
